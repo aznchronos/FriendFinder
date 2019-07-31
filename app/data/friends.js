be73d5e311560a7f61c1fbb3e142a540fe473085
Andy Ren <andyr914@gmail.com>
2019-07-30 21:18:40 -0700

    established questions and added more 'friend' variables

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,8 +1,28 @@
 var friends = [
     {
-        "name":"Ahmed",
-        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-        "scores":[5,1,4,4,5,1,2,5,4,1]
+        "name":"Mr. Grump",
+        "photo":"https://rlv.zcache.com/mr_grumpy_frowning_face_postcard-rdf4beec290034416ba9e031b19fd5471_vgbaq_8byvr_540.jpg",
+        "scores":[1,1,1,1,1,1,1,1,1,1]
+    },
+    {
+        "name":"Phil",
+        "photo":"https://m.media-amazon.com/images/M/MV5BMTg4ODc0NjkzM15BMl5BanBnXkFtZTcwMjI5NDY1Mg@@._V1_.jpg",
+        "scores":[3,2,3,4,5,5,1,3,5,4]
+    },
+    {
+        "name":"Stu",
+        "photo":"https://m.media-amazon.com/images/M/MV5BMTUzODE4MDY3M15BMl5BanBnXkFtZTcwOTM5NDY1Mg@@._V1_.jpg",
+        "scores":[4,4,2,4,5,5,3,2,4,3]
+    },
+    {
+        "name":"Alan",
+        "photo":"https://m.media-amazon.com/images/M/MV5BMTk0MjgxMzA5Nl5BMl5BanBnXkFtZTcwNjI5NDY1Mg@@._V1_.jpg",
+        "scores":[5,3,5,5,2,1,5,4,3,1]
+    },
+    {
+        "name":"Doug",
+        "photo":"https://m.media-amazon.com/images/M/MV5BMTk4NjI5NjIxN15BMl5BanBnXkFtZTcwNDI5NDY1Mg@@._V1_.jpg",
+        "scores":[2,4,3,1,4,2,2,2,1,5]
     }
 ]
 
